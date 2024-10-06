5feea0150a863d6259b52231abf852269235248a
nil <luiseniltonxy@gmail>
2024-10-06 18:01:51 -0300

    add structure

---
+++ banco.py
@@ -0,0 +1,24 @@
+class Banco (object):
+    def __init__(self):
+        #TODO: Implementar
+    
+    def inserir_cliente(self, cliente):
+        #TODO: Implementar
+    
+    def inserir_conta(self, conta):
+        #TODO: Implementar
+    
+    def autenticar(self, cliente):
+        #TODO: Implementar
+    
+    def deposito(self, conta, valor):
+        #TODO: Implementar
+    
+    def saque(self, conta, valor):
+        #TODO: Implementar
+    
+    def transferencia(self, conta_origem, conta_destino, valor):
+        #TODO: Implementar
+    
+    def saldo(self, conta):
+        #TODO: Implementar
