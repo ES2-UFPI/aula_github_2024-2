8ba8dc3b7bd4d97199ccf88cd81d34ca14d78f1c
Raffael <raffael13fernandes@gmail.com>
2024-10-06 18:24:30 -0300

    close issues #4 and #10

--- banco.py
+++ banco.py
@@ -1,24 +1,65 @@
+import re
+
 class Banco (object):
+    clientes = []
+    contas = []
+
     def __init__(self):
         #TODO: Implementar
+        return
     
     def inserir_cliente(self, cliente):
         #TODO: Implementar
+        return
     
-    def inserir_conta(self, conta):
-        #TODO: Implementar
+    def inserir_conta(self):
+        """
+        Insere uma nova conta no conjunto de contas.
+        Adiciona a conta fornecida ao conjunto de contas da instância atual.
+        Returns:
+            None
+        """
+        cpf = input("Digite o CPF do titular (somente números): ")
+        if not re.match(r'^\d{11}$', cpf):
+            print("CPF inválido. Deve conter exatamente 11 dígitos.")
+            return
+
+        nome = input("Digite o nome do titular: ")
+        if not re.match(r'^[A-Za-z\s]+$', nome):
+            print("Nome inválido. Deve conter apenas letras e espaços.")
+            return
+
+        try:
+            saldo_inicial = float(input("Digite o saldo inicial: "))
+        except ValueError:
+            print("Saldo inicial inválido. Deve ser um número.")
+            return
+
+        conta = {
+            'cpf': cpf,
+            'nome': nome,
+            'saldo': saldo_inicial
+        }
+
+        self.contas.add(conta)
+        return
     
     def autenticar(self, cliente):
         #TODO: Implementar
+        return
     
     def deposito(self, conta, valor):
         #TODO: Implementar
+        return
     
     def saque(self, conta, valor):
         #TODO: Implementar
+        return
     
     def transferencia(self, conta_origem, conta_destino, valor):
         #TODO: Implementar
+        return
     
     def saldo(self, conta):
         #TODO: Implementar
+        return
--- conta.py
+++ conta.py
@@ -1,18 +1,25 @@
 class Conta:
-    def init(self):
-        #TODO: Implementar
+    def init(self, cpf, titular, saldo=0):
+        self.cpf = cpf
+        self.titular = titular
+        self.saldo = saldo
+        
 
     def consultar_saldo(self):
         #Todo: Implementar
+        return
     
     def depositar(self, valor):
-        #Todo: Implementar
+        #Todo: Implementar]
+        return
         
     def sacar(self, valor):
         #Todo: Implementar
+        return
     
     def transferir(self, conta_destino, valor):
         #Todo: Implementar
+        return
     
     
     
